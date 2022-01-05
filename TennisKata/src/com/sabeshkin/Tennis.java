06a18cfa9548a197faf27e21f13a0899ca6d91c2
FedorSabeshkin <fsabeshkin@mail.ru>
2022-01-05 20:10:34 +0300

    add comments

--- TennisKata/src/com/sabeshkin/Tennis.java
+++ TennisKata/src/com/sabeshkin/Tennis.java
@@ -20,12 +20,18 @@ public class Tennis {
         tennis.printScoreResult(3,5);
     }
 
+    /**
+     * select Score Result in tennis system
+     * @param firstScore
+     * @param secondScore
+     * @return formated score result string
+     */
     public String selectScoreResult(int firstScore, int secondScore) {
 
         boolean isReachForty = isReachForty(firstScore, secondScore);
         if (isReachForty) {
-            boolean isEnoughScoreDiff = isEnoughScoreDiff(firstScore, secondScore);
-            if (isEnoughScoreDiff) {
+            boolean isWin = isWin(firstScore, secondScore);
+            if (isWin) {
                 return selectWinnerPlayerName(firstScore, secondScore);
             } else {
                 boolean isDeuce = isDeuce(firstScore, secondScore);
@@ -45,12 +51,23 @@ public class Tennis {
         }
     }
 
+
+    /**
+     * Print game scores in tennis system to terminal.
+     * @param firstScore
+     * @param secondScore
+     */
     public void printScoreResult(int firstScore, int secondScore) {
         String scoreResult = selectScoreResult(firstScore, secondScore);
         println(scoreResult);
     }
 
-
+    /**
+     * Select "name" leading player
+     * @param firstScore
+     * @param secondScore
+     * @return name of leading player
+     */
     public static String selectLeadingName(int firstScore, int secondScore) {
         if (firstScore > secondScore) {
             return "First";
@@ -59,31 +76,23 @@ public class Tennis {
         }
     }
 
-    public static boolean isNotEqualsScore(int firstScore, int secondScore) {
-        if (firstScore != secondScore) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
+    /**
+     *
+     * @param score
+     * @return score in tennis system
+     */
     public String selectTennisScores(int score) {
         return scoreAliases.get(score);
     }
 
+    /**
+     * somebady win or not
+     * @param firstScore
+     * @param secondScore
+     * @return
+     */
     public static boolean isWin(int firstScore, int secondScore) {
-        if (isReachForty(firstScore, secondScore)) {
-            if (!isDeuce(firstScore, secondScore)) {
-                return isEnoughScoreDiff(firstScore, secondScore);
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean isEnoughScoreDiff(int firstScore, int secondScore) {
         int scoreDifferent = Math.abs(firstScore - secondScore);
         if (scoreDifferent >= 2) {
             return true;
@@ -93,9 +102,56 @@ public class Tennis {
     }
 
 
+    /**
+     * Check what somebady already have 40 scores
+     * @param firstScore
+     * @param secondScore
+     * @return
+     */
+    public static boolean isReachForty(int firstScore, int secondScore) {
+        if (firstScore >= scoreForEnd || secondScore >= scoreForEnd) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check what somebady already have <b>more</b> than 40 scores
+     * @param firstScore
+     * @param secondScore
+     * @return
+     */
+    public static boolean isMoreForty(int firstScore, int secondScore) {
+        if (firstScore > scoreForEnd || secondScore > scoreForEnd) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Check deuce case. We should check isReachForty early.
+     * @param firstScore
+     * @param secondScore
+     * @return
+     */
+    public static boolean isDeuce(int firstScore, int secondScore) {
+        if (firstScore == secondScore) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Aliase for println function. Print line to terminal.
+     * @param message
+     */
     public static void println(String message) {
         System.out.println(message);
     }
+
 
 
     public String selectWinnerPlayerName(int firstScore, int secondScore) {
@@ -121,28 +177,4 @@ public class Tennis {
         return scoreResult;
     }
 
-
-    public static boolean isReachForty(int firstScore, int secondScore) {
-        if (firstScore >= scoreForEnd || secondScore >= scoreForEnd) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean isMoreForty(int firstScore, int secondScore) {
-        if (firstScore > scoreForEnd || secondScore > scoreForEnd) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static boolean isDeuce(int firstScore, int secondScore) {
-        if (firstScore == secondScore) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
