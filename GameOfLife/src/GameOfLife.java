acd50ba6d643e116b45b100e5f2a8eede9895c24
FedorSabeshkin <fsabeshkin@mail.ru>
2022-01-12 18:36:55 +0300

    add comments to methods

--- GameOfLife/src/GameOfLife.java
+++ GameOfLife/src/GameOfLife.java
@@ -7,10 +7,6 @@ public class GameOfLife {
     static int columnAmount = 4;
     static int rowAmount = 5;
 
-    // проверить что бы была иммутабельность при заполнении новых соседей,
-    // иначе можем слишком увеличить кол-во соседей у клетки
-    // что бы при создании новой таблицы обнулялось кол-во известных соседей у каждой клетки
-
 
     //вывод следующего поколения по входным данным
     public static void main(String args[]) {
@@ -31,7 +27,11 @@ public class GameOfLife {
     }
 
 
-    // печать двумерного массива
+    /**
+     * Печать двумерного массива в терминал
+     *
+     * @param chars
+     */
     public static void printTwoDimensionalArray(char[][] chars) {
         for (int i = 0; i < chars.length; i++) {
             for (int k = 0; k < chars[i].length; k++) {
@@ -41,7 +41,12 @@ public class GameOfLife {
         }
     }
 
-    // иницилизация сетки значениями из входных данных
+    /**
+     * Иницилизация сетки значениями из входных данных
+     *
+     * @param chars
+     * @return Двумерный массив Cell[][] содержащий отображения значков "*" и "." в объекты Cell
+     */
     public static Cell[][] charGridToCellGrid(char[][] chars) {
         Cell[][] grid = new Cell[columnAmount][rowAmount];
         //пробегаюсь в цикле по всему двумерному массиву символов
@@ -53,7 +58,12 @@ public class GameOfLife {
         return grid;
     }
 
-    // заполнение выходного массива символов данными из сетки
+    /**
+     * Заполнение выходного массива символов данными из сетки
+     *
+     * @param grid
+     * @return
+     */
     public static char[][] cellGridToCharGrid(Cell[][] grid) {
         char[][] chars = new char[columnAmount][rowAmount];
         //пробегаюсь в цикле по всему двумерному массиву символов
@@ -66,7 +76,12 @@ public class GameOfLife {
         return chars;
     }
 
-    // метод отображения объекта Cell в символы
+    /**
+     * Метод отображения объекта Cell в символы
+     *
+     * @param cell
+     * @return
+     */
     public static char cellToChar(@NotNull Cell cell) {
         boolean isLive = cell.getIsLive();
         if (isLive) {
@@ -78,7 +93,14 @@ public class GameOfLife {
     }
 
 
-    // метод отображения символа в объект Cell
+    /**
+     * Метод отображения символа в объект Cell
+     *
+     * @param symbol - символ для обозначения "живой" или "мертвой" клетки
+     * @param column - номер столбца в котором будет находится объект Cell в который отобразится символ
+     * @param row    - номер строки в котором будет находится объект Cell в который отобразится символ
+     * @return Объект Cell, находящейся в конкретной позиции, который является отображение входного символа
+     */
     public static Cell charToCell(char symbol, int column, int row) {
         if (symbol == '*') {
             return Cell.createLive(column, row);
@@ -88,7 +110,12 @@ public class GameOfLife {
 
     }
 
-    //Новое поколение сетки
+    /**
+     * Построение нового поколение сетки
+     *
+     * @param prevGenerationGrid - Cell[][] актуального поколения
+     * @return Cell[][] - следующего поколения
+     */
     public static Cell[][] generationalChange(Cell[][] prevGenerationGrid) {
         Cell[][] grid = prevGenerationGrid.clone();
         grid = calculateLiveNeighboursForAll(grid);
@@ -117,7 +144,12 @@ public class GameOfLife {
         return nextGenerationGrid;
     }
 
-    //Подсчет кол-ва соседей для всех точек
+    /**
+     * Подсчет кол-ва соседей для всех точек
+     *
+     * @param gridForCount
+     * @return Cell[][], где каждый обяъект Cell так же содержит информацию о кол-ве своих живых соседей
+     */
     public static Cell[][] calculateLiveNeighboursForAll(Cell[][] gridForCount) {
         Cell[][] grid = gridForCount.clone();
         for (int i = 0; i < grid.length; i++) {
@@ -133,7 +165,12 @@ public class GameOfLife {
     }
 
 
-    //будет ли "Мертва" в следующем поколении
+    /**
+     * Будет ли клетка "Мертва" в следующем поколении
+     *
+     * @param cell
+     * @return
+     */
     public static boolean isDeadNextGeneration(Cell cell) {
         int liveNeighbours = cell.getLiveNeighbours();
         if (liveNeighbours > 3 || liveNeighbours < 2) {
@@ -143,7 +180,12 @@ public class GameOfLife {
         }
     }
 
-    //будет ли "Жива" в следующем поколении
+    /**
+     * Будет ли клетка "Жива" в следующем поколении
+     *
+     * @param cell
+     * @return
+     */
     public static boolean isLiveNextGeneration(Cell cell) {
         int liveNeighbours = cell.getLiveNeighbours();
         if (liveNeighbours == 3) {
@@ -153,8 +195,12 @@ public class GameOfLife {
         }
     }
 
-    // Здесь ошибка
-    // оставляем в том же состоянии, в котором была
+    /**
+     * Останется ли клетка в том же состоянии в следующем поколении
+     *
+     * @param cell
+     * @return
+     */
     public static boolean isSameNextGeneration(Cell cell) {
         int liveNeighbours = cell.getLiveNeighbours();
         if (liveNeighbours == 2 || liveNeighbours == 3) {
@@ -164,7 +210,13 @@ public class GameOfLife {
         }
     }
 
-    //Подсчет кол-ва соседей для конкретной точки
+    /**
+     * Подсчет живых кол-ва соседей для конкретной точки
+     *
+     * @param cell клетка
+     * @param grid сетка в которой она находится
+     * @return кол-во живых соседей для точки
+     */
     public static int calculateLiveNeighbours(Cell cell, Cell[][] grid) {
         int[][] neigbourPoints = generateNeighbourPoints(cell);
         int liveNeighbours = 0;
@@ -182,7 +234,12 @@ public class GameOfLife {
         return liveNeighbours;
     }
 
-    // generate Neighbour Points
+    /**
+     * Generate Neighbour Points
+     *
+     * @param cell
+     * @return
+     */
     public static int[][] generateNeighbourPoints(@NotNull Cell cell) {
         int row = cell.getRow();
         int column = cell.getColumn();
@@ -210,13 +267,22 @@ public class GameOfLife {
 
 
     /**
-     * проверка что сосед живой
+     * Провереят, что сосед клетки "жив"
+     *
+     * @param cell
+     * @return
      */
     public static boolean isLiveNeighbour(@NotNull Cell cell) {
         return cell.getIsLive();
     }
 
-    // проверка, что точки находятся в границах поля
+    /**
+     * Проверяет, что точки находятся в границах поля
+     *
+     * @param column
+     * @param row
+     * @return
+     */
     public static boolean isValidPoints(int column, int row) {
         if (isValidColumn(column) && isValidRow(row)) {
             return true;
@@ -225,7 +291,12 @@ public class GameOfLife {
         }
     }
 
-
+    /**
+     * Проверяет что значение колонки не выходит за пределы сетки
+     *
+     * @param column
+     * @return
+     */
     @Contract(pure = true)
     public static boolean isValidColumn(int column) {
         if (column < columnAmount && column >= 0) {
@@ -235,6 +306,12 @@ public class GameOfLife {
         }
     }
 
+    /**
+     * Проверяет что значение строки не выходит за пределы сетки
+     *
+     * @param row
+     * @return
+     */
     @Contract(pure = true)
     public static boolean isValidRow(int row) {
         if (row < rowAmount && row >= 0) {
