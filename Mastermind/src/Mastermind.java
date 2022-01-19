51806aa3c8e7766ce34eec67e83247dc4c60cf59
FedorSabeshkin <fsabeshkin@mail.ru>
2022-01-19 17:30:29 +0300

    add countSamePositionColors()

--- Mastermind/src/Mastermind.java
+++ Mastermind/src/Mastermind.java
@@ -3,13 +3,39 @@ import java.util.Arrays;
 public class Mastermind {
 
 
+    private String[] paternColors;
+
+    public Mastermind startGame(String[] paternColors){
+        return new Mastermind(paternColors);
+    }
+
+    private Mastermind(String[] paternColors){
+        this.paternColors = paternColors;
+    }
+
     public static void main(String args[]) {
         System.out.println();
     }
 
-    public static boolean checkColorExist(String inColor, String[] paternColors){
+    // проверяем, что цвет содержится в массиве
+    public static boolean checkColorExist(String inColor, String[] patternColors){
         // пробежаться по массиву и найти первое соответствие, можно сделать через Stream
-        Optional<String> sameColor = Arrays.stream(paternColors).filter(color->color.equals(inColor)).findFirst();
+        Optional<String> sameColor = Arrays.stream(patternColors).filter(color->color.equals(inColor)).findFirst();
         return sameColor.isPresent();
+    }
+
+    // подсчитывает кол-во совпадающих цветов в одинаковых позициях в массивах
+    public static int countSamePositionColors(String[] patternColors, String[] inColors) {
+        // просто в цикле по основному массиву проверяем совпадает ли элемент
+        // с находящемся во входящем массиве в той же позиции
+        // и в случае успеха итерируем переменную
+        int amountInSamePosition = 0;
+        for(int i=0; i<patternColors.length; i++){
+            //if equals colors
+            if(patternColors[i].equals(inColors[i])){
+                amountInSamePosition++;
+            }
+        }
+        return amountInSamePosition;
     }
 }
--- Mastermind/src/MastermindTests.java
+++ Mastermind/src/MastermindTests.java
@@ -19,4 +19,28 @@ public class MastermindTests {
     public void checkColorExist_Last_Item_True() {
         assertTrue(Mastermind.checkColorExist("black", new String[] {"blue","yellow","black"}));
     }
+
+    @Test
+    public void countSamePositionColors_1() {
+        assertEquals(1,
+                Mastermind.countSamePositionColors(
+                new String[] {"red","blue","orange"},
+                new String[] {"yellow","blue","black"}));
+    }
+
+    @Test
+    public void countSamePositionColors_0() {
+        assertEquals(0,
+                Mastermind.countSamePositionColors(
+                new String[] {"red","blue","orange"},
+                new String[] {"green","yellow","black"}));
+    }
+
+    @Test
+    public void countSamePositionColors_3() {
+        assertEquals(3,
+                Mastermind.countSamePositionColors(
+                new String[] {"red","blue","orange"},
+                new String[] {"red","blue","orange"}));
+    }
 }
