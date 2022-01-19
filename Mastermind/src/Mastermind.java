c308401f26388d0d08b993fbd0093f4ea2e8449a
FedorSabeshkin <fsabeshkin@mail.ru>
2022-01-19 17:00:17 +0300

    add checkColorExist()

--- Mastermind/src/Mastermind.java
+++ Mastermind/src/Mastermind.java
@@ -1,3 +1,15 @@
+import java.util.*;
+import java.util.Arrays;
 public class Mastermind {
 
+
+    public static void main(String args[]) {
+        System.out.println();
+    }
+
+    public static boolean checkColorExist(String inColor, String[] paternColors){
+        // пробежаться по массиву и найти первое соответствие, можно сделать через Stream
+        Optional<String> sameColor = Arrays.stream(paternColors).filter(color->color.equals(inColor)).findFirst();
+        return sameColor.isPresent();
+    }
 }
--- Mastermind/src/MastermindTests.java
+++ Mastermind/src/MastermindTests.java
@@ -1,2 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.*;
+
 public class MastermindTests {
+
+    @Test
+    public void checkColorExist_True() {
+        assertTrue(Mastermind.checkColorExist("blue", new String[] {"blue","yellow","black"}));
+    }
+
+    @Test
+    public void checkColorExist_False() {
+        assertFalse(Mastermind.checkColorExist("red", new String[] {"blue","yellow","black"}));
+    }
+
+    @Test
+    public void checkColorExist_Last_Item_True() {
+        assertTrue(Mastermind.checkColorExist("black", new String[] {"blue","yellow","black"}));
+    }
 }
