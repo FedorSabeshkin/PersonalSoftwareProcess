889aefc202204842e0bda8318eab4eaf351f28ec
FedorSabeshkin <fsabeshkin@mail.ru>
2022-01-30 23:16:25 +0300

    add yahtzee()

--- YahtzeeScores/src/com/sabeshkin/YahtzeeCounter.java
+++ YahtzeeScores/src/com/sabeshkin/YahtzeeCounter.java
@@ -13,15 +13,18 @@ public class YahtzeeCounter {
             int amountOfMatch = countMatch(Constants.combination.get(combination), dices);
             return amountOfMatch * Constants.combination.get(combination);
         }
-        switch (combination){
-            case "threeOfKind":{
+        switch (combination) {
+            case "threeOfKind": {
                 return threeOfKind(dices);
             }
-            case "fourOfKind":{
+            case "fourOfKind": {
                 return fourOfKind(dices);
             }
-            case "fullHouse":{
-                fullHouse(dices);
+            case "fullHouse": {
+                return fullHouse(dices);
+            }
+            case "yahtzee": {
+                return yahtzee(dices);
             }
         }
         return 0;
@@ -29,18 +32,19 @@ public class YahtzeeCounter {
 
     /**
      * Count full house scores
+     *
      * @param dices combination of dices
      * @return scores for full house
      */
-    public static int fullHouse(int[] dices){
+    public static int fullHouse(int[] dices) {
         for (int i = 1; i <= Constants.maxDice; i++) {
             // можно заменить на isNeedAmountOfMatch(remainDices,3)
             int amountOfMatch = countMatch(i, dices);
-            if(amountOfMatch==3){
-                
+            if (amountOfMatch == 3) {
+
                 int[] remainDices = removeFromDices(dices, i);
-                boolean isTwoMatches = isNeedAmountOfMatch(remainDices,2);
-                if(isTwoMatches){
+                boolean isTwoMatches = isNeedAmountOfMatch(remainDices, 2);
+                if (isTwoMatches) {
                     return Constants.fullHouseScores;
                 }
             }
@@ -48,26 +52,36 @@ public class YahtzeeCounter {
         return 0;
     }
 
+    public static int yahtzee(int[] dices) {
+        if (isNeedAmountOfMatch(dices, 5)) {
+            return 50;
+        } else {
+            return 0;
+        }
+    }
+
     /**
      * Remove selected dice from combination of dices
-     * @param dices combination of dices
+     *
+     * @param dices    combination of dices
      * @param diceType dice for remove
      * @return combination of dices without diceType
      */
-    public static int[] removeFromDices(int[] dices, int diceType){
-        return Arrays.stream(dices).filter(dice -> dice!=diceType).toArray();
+    public static int[] removeFromDices(int[] dices, int diceType) {
+        return Arrays.stream(dices).filter(dice -> dice != diceType).toArray();
     }
 
     /**
      * Check need amount of match
-     * @param dices combination
+     *
+     * @param dices      combination
      * @param needAmount need amount of match
      * @return reach need amount of match or not
      */
     public static boolean isNeedAmountOfMatch(int[] dices, int needAmount) {
         for (int i = 1; i <= Constants.maxDice; i++) {
             int amountOfMatch = countMatch(i, dices);
-            if(amountOfMatch==needAmount){
+            if (amountOfMatch == needAmount) {
                 return true;
             }
         }
@@ -76,13 +90,13 @@ public class YahtzeeCounter {
 
     /**
      * Count scores for different cases (four/three)
-     * @param dices combination
+     *
+     * @param dices      combination
      * @param needAmount need amount of match
      * @return count scores for different cases (four/three)
      */
     //в идеале сюда надо передавать вторым аргументом enum, у которого строго ограничен набор значений
     //так же поможет избавиться от констант в коде
-
     public static int needAmountOfKind(int[] dices, int needAmount) {
         int maxAmountMatches = maxAmountMatches(dices);
         if (maxAmountMatches >= needAmount) {
@@ -94,6 +108,7 @@ public class YahtzeeCounter {
 
     /**
      * count scores for four Of Kind
+     *
      * @param dices combination
      * @return count scores for four Of Kind
      */
@@ -104,6 +119,7 @@ public class YahtzeeCounter {
 
     /**
      * count scores for three Of Kind
+     *
      * @param dices combination
      * @return count scores for three Of Kind
      */
